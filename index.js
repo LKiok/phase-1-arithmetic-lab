7079419248a0d7a2e0c5d8f73bf5f47403d4490b
LKiok <kiokletia720@gmail.com>
2023-06-13 21:16:18 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1  =31
+let num2 =2
+let multiply =num1 * num2
+let random = Math.round(Math.random()) + 1
+let num3 =60
+let num4 =7
+const mod =num3 % num4
+const max =Math.max(4,5,8,10,20)
